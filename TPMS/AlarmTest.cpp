14ee447fbbcb1225f1b08f3ce38a37ffb6029751
Ben Wu <wubin28@gmail.com>
2014-07-16 23:18:23 +0800

    Wrote the intention code for the Act and Arrange parts of the test a_normal_pressure_value_should_not_raise_the_alarm.

--- TPMS/AlarmTest.cpp
+++ TPMS/AlarmTest.cpp
@@ -2,6 +2,13 @@
  
 // TODO-intent: a normal pressure value should not raise the alarm
 TEST(AlarmTest, a_normal_pressure_value_should_not_raise_the_alarm) {
+  // Arrange
+  StubSensor *stubSensor = new StubSensor;
+  stubSensor->arrangeNextPressureValue(Alarm::HIGH_PRESSURE_THRESHOLD);
+  Alarm *alarm = new Alarm(stubSensor);
+  
+  // Act
+  alarm->check();
   
   // Assert
   EXPECT_FALSE(alarm->isAlarmOn());

504b302913a3249cbb6462435289e33c813a493c
Ben Wu <wubin28@gmail.com>
2014-07-16 23:00:35 +0800

    Added 2 intent TODOs.

--- TPMS/AlarmTest.cpp
+++ TPMS/AlarmTest.cpp
@@ -5,3 +5,5 @@ TEST(AlarmTest, a_trivial_test) {
 }
 
 // TODO-new-intent: a normal pressure value after a value outside the range should stop the alarm
+// TODO-intent: a normal pressure value should not raise the alarm
+// TODO-intent: a pressure value outside the range should raise the alarm

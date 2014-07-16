ea1aafa4fed9575b0fcc9883950cbc79acffea0e
Ben Wu <wubin28@gmail.com>
2014-07-16 23:09:39 +0800

    Added test a_normal_pressure_value_should_not_raise_the_alarm with an Assert part.

--- TPMS/AlarmTest.cpp
+++ TPMS/AlarmTest.cpp
@@ -1,9 +1,11 @@
 #include "gtest/gtest.h"
  
-TEST(AlarmTest, a_trivial_test) {
-    EXPECT_EQ(5, 3 + 2);
+// TODO-intent: a normal pressure value should not raise the alarm
+TEST(AlarmTest, a_normal_pressure_value_should_not_raise_the_alarm) {
+  
+  // Assert
+  EXPECT_FALSE(alarm->isAlarmOn());
 }
 
-// TODO-new-intent: a normal pressure value after a value outside the range should stop the alarm
-// TODO-intent: a normal pressure value should not raise the alarm
 // TODO-intent: a pressure value outside the range should raise the alarm
+// TODO-new-intent: a normal pressure value after a value outside the range should stop the alarm

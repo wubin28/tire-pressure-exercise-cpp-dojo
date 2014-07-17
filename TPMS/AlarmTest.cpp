6504f7dcfb594668c2fbd3e8864482546f9381a4
Ben Wu <wubin28@gmail.com>
2014-07-17 22:25:41 +0800

    Wrote the Act and Arrange parts of test a_normal_pressure_value_after_a_value_outside_the_range_should_stop_the_alarm.

--- TPMS/AlarmTest.cpp
+++ TPMS/AlarmTest.cpp
@@ -30,6 +30,15 @@ TEST(AlarmTest, a_pressure_value_outside_the_range_should_raise_the_alarm) {
 
 // TODO-new-intent-working-on: a normal pressure value after a value outside the range should stop the alarm
 TEST(AlarmTest, a_normal_pressure_value_after_a_value_outside_the_range_should_stop_the_alarm) {
+  // Arrange
+  StubSensor *stubSensor = new StubSensor;
+  stubSensor-arrangeNextTwoPressureValues(Alarm::HIGH_PRESSURE_THRESHOLD + 1, Alarm::HIGH_PRESSURE_THRESHOLD);
+  Alarm *alarm = new Alarm(stubSensor);
+  
+  // Act
+  alarm->check();
+  alarm->check();
+  
   // Assert
   EXPECT_FALSE(alarm->isAlarmOn());
 }

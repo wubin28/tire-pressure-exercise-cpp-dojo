fbcb5f4318fa55a12f447fd2c66963cffb53a73f
Ben Wu <wubin28@gmail.com>
2014-07-17 22:09:09 +0800

    Added test a_pressure_value_outside_the_range_should_raise_the_alarm with the Assert part of it.

--- TPMS/AlarmTest.cpp
+++ TPMS/AlarmTest.cpp
@@ -16,5 +16,9 @@ TEST(AlarmTest, a_normal_pressure_value_should_not_raise_the_alarm) {
 }
 
 // TODO-intent-working-on: a pressure value outside the range should raise the alarm
+TEST(AlarmTest, a_pressure_value_outside_the_range_should_raise_the_alarm) {
+  // Assert
+  EXPECT_TRUE(alarm->isAlarmOn());
+}
 
 // TODO-new-intent: a normal pressure value after a value outside the range should stop the alarm
